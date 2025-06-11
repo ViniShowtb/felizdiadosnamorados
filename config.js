63dce4864e01e2e587d0cb4455a5e8f8809e71f5
ViniShowtb <vinigrafias@gmail.com>
2025-06-11 10:04:48 -0300

    Update config.js

--- config.js
+++ config.js
@@ -42,9 +42,9 @@ const CONFIG = {
     // Love meter messages
     // They show up depending on how far they slide the meter
     loveMessages: {
-        extreme: "WOOOOW You love me that much?? 🥰🚀💝",  // Shows when they go past 5000%
-        high: "To infinity and beyond! 🚀💝",              // Shows when they go past 1000%
-        normal: "And beyond! 🥰"                           // Shows when they go past 100%
+        extreme: "Me ama demais ein, mas te amo mais! 🥰🚀💝",  // Shows when they go past 5000%
+        high: "Slc, me ama demais! 🚀💝",              // Shows when they go past 1000%
+        normal: " Te amo muito mais que isso! 🥰"                           // Shows when they go past 100%
     },
 
     // Messages that appear after they say "Yes!"
